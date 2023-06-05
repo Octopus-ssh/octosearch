4d889ca90f7ccc6b44f364c26cb8cc5b8e784b7f
Octopus-ssh <85446795+Octopus-ssh@users.noreply.github.com>
2023-06-05 17:58:52 +0200

    Change 2.2
    Changing about the enumeration

--- enumeration.py
+++ enumeration.py
@@ -13,19 +13,18 @@ def check_url(url, path):
         # Ignore any exceptions raised by the request
         pass
 
+# Function for enumeration
 def enumeration(url, wordlist):
-    # Print a message indicating the start of the directory enumeration
-    print("[+] enumeration is starting...\n\n")
-    with open(wordlist, "r") as file:
-        # Create a generator that yields each line of the file, stripping any leading or trailing whitespace
+    print("[+] Enumeration is about to start...\n")
+
+    # Open the wordlist file with UTF-8 encoding, ignoring errors
+    with open(wordlist, "r", encoding="utf-8", errors='ignore') as file:
+        # Read and strip each line in the file to get paths
         paths = (line.strip() for line in file)
+
+        # Create a ThreadPoolExecutor with 100 maximum workers
         with concurrent.futures.ThreadPoolExecutor(max_workers=100) as executor:
-            # Submit a task to the executor for each path in the wordlist
-            futures = {executor.submit(check_url, url, path): path for path in paths}
-            # Process the tasks as they complete
-            for future in concurrent.futures.as_completed(futures):
-                # Get the path associated with the completed task
-                path = futures[future]
-                # Ignore any exceptions raised by the task
-                if future.exception() is not None:
-                    continue
+            futures = []
+            # Submit tasks for each path in the wordlist
+            for path in paths:
+                future = executor.submit(check_url, url, path)
